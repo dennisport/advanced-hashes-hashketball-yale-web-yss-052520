008c01a1b028245eaaf00a17a12fa477d4af47a7
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-24 02:13:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,9 @@ def player_numbers(team_name)
     new_array = []
 game_hash.each do |team, team_data|       
  if team_data[:team_name] == team_name 
-   team_data[:players].each do []
+   team_data[:players].each do [player]
+ end
+ new_array << (player[:number])
 end
 
 def player_stats(player_name)
